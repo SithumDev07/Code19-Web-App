71564f98d828fece736750494ca3f94290912fdf
Sithum Basnayaka <sithum.dashantha@gmail.com>
2021-08-02 16:55:24 +0530

    Setup Tailwindcss And HomePage

---
+++ layouts/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Restaurant Management</title>
+    <link rel="stylesheet" href="./public/style.css">
+    <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+</head>
+
+<body>
