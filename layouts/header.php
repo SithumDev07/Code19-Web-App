9e2a9adc03540cd27e0749e84522c5d7b03e4b63
Sithum Basnayaka <sithum.dashantha@gmail.com>
2021-08-03 00:29:11 +0530

    Form Validation for register staff First Form

--- layouts/header.php
+++ layouts/header.php
@@ -5,20 +5,35 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Restaurant Management</title>
+    <title>Login</title>
     <link rel="stylesheet" href="./public/style.css">
-    <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+
+    <style>
+        .success {
+            border-color: rgba(16, 185, 129, 1);
+        }
+
+        .error {
+            border-color: rgba(239, 68, 68, 1);
+        }
+
+        .fa-check-circle {
+            visibility: hidden;
+        }
+
+        .fa-exclamation-circle {
+            visibility: hidden;
+        }
+    </style>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 
 <body>
-
     <main class="bg-white">
-        <div class="bg-white border-b fixed top-0 left-0 right-0">
+        <div class="bg-white border-b fixed top-0 left-0 right-0 z-20">
             <nav class="h-16 container mx-auto flex">
                 <div class="logo w-20 h-16 p-2 mr-10 flex items-center justify-center">
-                    <a href="../index.php">
-                        <img class="w-full h-full object-contain" src="https://cdn.worldvectorlogo.com/logos/tailwindcss.svg" alt="Logo">
-                    </a>
+                    <img class="w-full h-full object-contain" src="https://cdn.worldvectorlogo.com/logos/tailwindcss.svg" alt="Logo">
                 </div>
                 <ul class="text-gray-600 flex flex-1 justify-end items-center">
                     <li class="ml-8">
--- scripts/signup.js
+++ scripts/signup.js
@@ -1,3 +1,115 @@
-// function isEmpty(input) {
-//     if(input)
-// }
+const form = document.querySelector('form');
+const username = document.querySelector('#username');
+const email = document.querySelector('#email');
+const password = document.querySelector('#password');
+const confirmedPassword = document.querySelector('#confirmedPassword');
+const submit = document.querySelector('#submit');
+
+// submit.addEventListener('submit', (e) => {
+//     //Prevent form auto submitting
+//     e.preventDefault();
+
+//     if(!checkInputs()) {
+//         return;
+//     }
+    
+//     console.log(checkInputs());
+// })
+
+if(submit.addEventListener) {
+    submit.addEventListener('click', returnToPrevious);
+} else {
+    submit.attachEvent('onclick', returnToPrevious);
+}
+
+function returnToPrevious (e) {
+    e = e || window.event;
+
+    if(!checkInputs()) {
+        if(e.preventDefault) {
+            e.preventDefault();
+        } else {
+            e.returnValue = false;
+        }
+    }
+
+}
+
+
+function checkInputs() {
+    // e.preventDefault();
+    //Getting all the values
+    const usernameValue = username.value.trim();
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+    const confirmedPasswordValue = confirmedPassword.value.trim();
+    var success = true;
+
+    if(usernameValue === '') {
+        setError(username, 'Username is required');
+        success =  false;
+    } else {
+        setSuccess(username);
+    }
+
+    if(emailValue === '') {
+        setError(email, 'Email is required');
+        success =  false;
+    } else if (!isEmail(emailValue)) {
+        setError(email, 'Email is not valid');
+        success =  false;
+    } else {
+        setSuccess(email);
+    }
+
+    if(passwordValue === '') {
+        setError(password, 'Password is required');
+        success =  false;
+    } else {
+        setSuccess(password);
+    }
+
+    if(confirmedPasswordValue === '') {
+        setError(confirmedPassword, 'Please confirm your password');
+        success =  false;
+    } else if (confirmedPasswordValue !== passwordValue) {
+        setError(confirmedPassword, "Passwords don't match");
+        success =  false;
+    } else {
+        setSuccess(confirmedPassword);
+    }
+
+    return success;
+
+}
+
+function setError(input, message) {
+    const formControl = input.parentElement; //Parent of input
+    const errorEl = formControl.querySelector('p');
+    const errorIcon = formControl.querySelector('.fa-exclamation-circle');
+
+    errorEl.innerText = message;
+    errorEl.classList.remove('hidden');
+    input.classList.add('error');
+    errorIcon.style.visibility = 'visible';
+}
+
+function setSuccess (input) {
+    const formControl = input.parentElement;
+
+    const errorEl = formControl.querySelector('p');
+    const errorIcon = formControl.querySelector('.fa-exclamation-circle');
+    const successIcon = formControl.querySelector('.fa-check-circle');
+
+    errorEl.classList.add('hidden');
+    input.classList.add('success');
+    input.classList.remove('error');
+    errorIcon.style.visibility = 'hidden';
+    successIcon.style.visibility = 'visible';
+}
+
+
+function isEmail (email) {
+    // RegEx for test for a valid email
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
