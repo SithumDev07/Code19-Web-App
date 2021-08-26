cadb8c3efabf1cfb821221c7784e5376db5d4397
Sithum Basnayaka <sithum.dashantha@gmail.com>
2021-08-26 09:15:12 +0530

    Quantity Changer on customize menu

--- operations/get-food-customize-data.php
+++ operations/get-food-customize-data.php
@@ -8,6 +8,34 @@ function Render($results, $conn)
 
     while ($row = mysqli_fetch_assoc($results)) {
 ?>
+
+        <div class="popupmenu scale-0 w-96 h-56 rounded-md shadow-xl bg-gray-100 bg-opacity-80 fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50 transition duration-200">
+            <button class="fixed h-8 w-8 rounded-full bg-black top-1 md:top-4 right-1 md:right-4 flex justify-center items-center text-white z-50 transform transition active:scale-90 duration-150" id="CloseConfirmMenu">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                </svg>
+            </button>
+            <div class="flex items-center flex-col justify-center h-full">
+                <h2 class="text-center text-2xl font-semibold text-gray-800">Confirmation</h2>
+                <p class="text-sm text-gray-500 text-center my-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, dicta!</p>
+
+                <div class="flex items-center w-full px-5 mt-3">
+                    <button type="submit" class="relative w-full rounded-md mr-3 flex flex-1 items-center justify-center bg-yellow-500 p-3 text-gray-300 text-sm font-semibold h-14 hover:bg-yellow-600 transition transform duration-200 active:scale-90" id="takeAway">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z" />
+                        </svg>
+                        Take away
+                    </button>
+                    <button type="submit" class="relative w-full rounded-md flex flex-1 items-center justify-center bg-green-500 p-3 text-gray-300 text-sm font-semibold h-14 hover:bg-green-600 transition transform duration-200 active:scale-90" id="Proceed">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z" />
+                        </svg>
+                        Add to cart
+                    </button>
+                </div>
+            </div>
+        </div>
+
         <button class="fixed h-14 w-14 rounded-full bg-black top-1 md:top-6 right-1 md:right-8 flex justify-center items-center text-white z-50 transform transition active:scale-90 duration-150" id="CloseCustomMenu">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
@@ -48,22 +76,22 @@ function Render($results, $conn)
                                 <?php echo $rowFilling['name'];  ?>
                             </button>
 
-                        <?php
+                    <?php
 
                         }
                     }
-                ?>
+                    ?>
 
                 </div>
                 <div class="flex justify-between items-center">
                     <div class="p-3 w-auto h-auto flex items-center text-gray-200 active:scale-90 mt-3 text-xs">
-                        <button class="transition duration-150 hover:shadow-lg">
+                        <button class="transition duration-150 hover:shadow-lg transform active:scale-90" id="QuantityIncreaser">
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10 bg-black p-1 rounded-full" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
                             </svg>
                         </button>
-                        <p class="mx-3 font-bold text-2xl">0</p>
-                        <button class="transition duration-150 hover:shadow-lg">
+                        <p class="mx-3 font-bold text-2xl quantity-customize">0</p>
+                        <button class="transition duration-150 hover:shadow-lg transform active:scale-90" id="QuantityReducer">
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10 bg-black p-1 rounded-full" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18 12H6" />
                             </svg>
@@ -71,7 +99,8 @@ function Render($results, $conn)
                     </div>
                     <h2 class="text-4xl text-gray-100 font-semibold">Rs.<?php echo $row['basic_price']; ?>/<span class="text-sm">each</span></h2>
                 </div>
-                <button disabled class="rounded-br-none fixed bottom-5 right-10 explore flex text-gray-100 bg-black py-3 px-5 rounded-xl justify-center items-center mt-5 font-semibold disabled:opacity-50" id="checkout" onclick="goCheckout()">Add to cart<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 ml-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <!-- // TODO Take care of disabled -->
+                <button disabled class="rounded-br-none fixed bottom-5 right-10 explore flex text-gray-100 bg-black py-3 px-5 rounded-xl justify-center items-center mt-5 font-semibold disabled:opacity-50" id="GoCheckout">Add to cart<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 ml-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8l4 4m0 0l-4 4m4-4H3" />
                     </svg></button>
 
--- scripts/food-customize.js
+++ scripts/food-customize.js
@@ -2,6 +2,7 @@ let foodCards = document.querySelectorAll('.card-food');
 
 $(document).ready(function() {
     let foodId;
+    let proceedCheck = 0;
     foodCards.forEach((ele,index) => {
         $(ele).click(function() {
             $(".customize-menu").removeClass("scale-0");
@@ -17,12 +18,53 @@ $(document).ready(function() {
         })
     })
     toggleText = true;
-  
-    
     function customizeMenuHandler (foodId) {
+        $(".quantity-customize").change(function() {
+            if(proceedCheck !== 0) {
+                document.querySelector("#GoCheckout").removeAttribute('disabled');
+            } else {
+                document.querySelector("#GoCheckout").setAttribute('disabled', '');
+            }
+        })
+
         $("#CloseCustomMenu").click(function() {
             $(".customize-menu").addClass("scale-0");
             $("body").removeClass("overflow-hidden");
         })
+
+        // ? Checkout
+        $("#GoCheckout").click(function() {
+            $(".popupmenu").removeClass('scale-0');
+        })
+        
+        // ? Close Confirmation
+        $("#CloseConfirmMenu").click(function () {
+            $(".popupmenu").addClass('scale-0');
+        })
+
+        // ? Increasing Quantity
+        $("#QuantityIncreaser").click(function() {
+            document.querySelector(".quantity-customize").innerHTML = parseInt(document.querySelector(".quantity-customize").innerHTML) + 1;
+            proceedCheck = parseInt(document.querySelector(".quantity-customize").innerHTML);
+            // listenOnProceed();
+            listenOnProceed();
+        })
+        
+        // ? Decreasing Quantity
+        $("#QuantityReducer").click(function() {
+            if(parseInt(document.querySelector(".quantity-customize").innerHTML) >= 1)
+            document.querySelector(".quantity-customize").innerHTML = parseInt(document.querySelector(".quantity-customize").innerHTML) - 1;
+            proceedCheck = parseInt(document.querySelector(".quantity-customize").innerHTML);
+            // listenOnProceed();
+            listenOnProceed();
+        })
+    }
+
+    function listenOnProceed() {
+        if(proceedCheck !== 0) {
+            document.querySelector("#GoCheckout").removeAttribute('disabled');
+        } else {
+            document.querySelector("#GoCheckout").setAttribute('disabled', '');
+        }
     }
 })
